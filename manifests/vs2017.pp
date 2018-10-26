0c4fe8b0d5307875fea13d7caf792dd4dbd52f8c
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2018-10-26 15:34:31 +0100

    Allow setting up SSDT BI for VS 2017.

--- manifests/vs2017.pp
+++ manifests/vs2017.pp
@@ -7,11 +7,14 @@
 # $components: An array of ids of components/workloads to be installed.
 #   default to ['Microsoft.VisualStudio.Workload.VisualStudioExtension', 'Microsoft.VisualStudio.Component.Web']
 #
+# $ssdtbi_version: The version of "SQL Server Data Tools - Visual Studio 2017" to install as displayed in "Program Features".
+#                  Note that this will only work properly if only 1 instance/edition of VS 2017 is installed.
 class visualstudio::vs2017(
   $editions = ['Professional'],
-  $components = ['Microsoft.VisualStudio.Workload.VisualStudioExtension', 'Microsoft.VisualStudio.Component.Web']
+  $components = ['Microsoft.VisualStudio.Workload.VisualStudioExtension', 'Microsoft.VisualStudio.Component.Web'],
+  $ssdtbi_version = undef,
   ) {
-  
+
   if !$editions.is_a(Array) {
     fail('The editions parameter expects an array')
   }
@@ -33,4 +36,28 @@ class visualstudio::vs2017(
   # Only set the environment variable when all the VS components have
   # been successfully installed.
   Visualstudio::Vs2017::Component <| |> -> Windows_env['VISUALSTUDIO_VERSION=2017']
+
+  if $ssdtbi_version {
+    $ssdtbi_urls = {
+      '14.0.16179.0' => 'https://go.microsoft.com/fwlink/?linkid=2024393',
+    }
+
+    $url = $ssdtbi_urls[$ssdtbi_version]
+    if $url == undef {
+      fail("visualstudio::vs2017 does not know where to download SSDT BI v${ssdtbi_version}")
+    }
+
+    require archive
+    archive { "C:/Windows/Temp/SSDT-Setup-ENU-${ssdtbi_version}.exe":
+      source => $url,
+    }
+    -> package { 'Microsoft SQL Server Data Tools - Visual Studio 2017':
+      ensure          => $ssdtbi_version,
+      source          => "C:/Windows/Temp/SSDT-Setup-ENU-${ssdtbi_version}.exe",
+      install_options => ['/quiet', '/norestart', '/INSTALLALL'],
+      require         => Visualstudio::Vs2017::Installer[$editions],
+    }
+
+    Package['Microsoft SQL Server Data Tools - Visual Studio 2017'] -> Windows_env['VISUALSTUDIO_VERSION=2017']
+  }
 }
--- spec/manifests/vs2017.pp
+++ spec/manifests/vs2017.pp
@@ -1,3 +1,5 @@
 class{ '::visualstudio::vs2017':
-  editions => ['Professional', 'Community']
+  editions       => ['Professional'],
+  components     => ['Microsoft.VisualStudio.Component.Web'],
+  ssdtbi_version => '14.0.16179.0',
 }
